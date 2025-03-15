54b35ac67a66fd72021805f9a32ba5e7c75174d1
parthiban1417 <parthiban1315@gmail.com>
2025-03-15 18:04:15 +0530

    Initial commit with exp & setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube_comment_sentiment_analysis',
+    version= '0.0.1',
+    author= 'Parthiban',
+    author_email= 'parthiban1315@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
